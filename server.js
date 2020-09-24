ee880e9bb6b55ba5cd5c7b3f200727029524d84c
Eric Zanchettin Presto <canalmontrisz@hotmail.com>
2020-09-23 21:09:19 -0300

    feature: Added router

--- server.js
+++ server.js
@@ -1,10 +1,8 @@
 const express = require('express');
-
 const server = express();
+const routes = require('./src/routes/routes');
 
-server.get('/', (req, res) => {
-    res.send('Hello World!')
-});
+server.use(routes);
 
 server.listen(3333, () => {
     console.log('Server is up and Running ...');
